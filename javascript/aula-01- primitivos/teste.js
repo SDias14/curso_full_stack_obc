0e67c523757e673bae246afc735d99a9868df209
samuel dias <samuel.dias36@etec.sp.gov.br>
2022-06-29 16:37:41 -0300

    aula 01 e 02

---
+++ javascript/aula-01- primitivos/teste.js
@@ -0,0 +1,27 @@
+//tipo number : representação de números, todos os numeros incluidos. 
+
+//console.log(typeof 10);
+
+//string : cadeia de caracteres. Pode ser  representado por uma aspas simples ou duplas, ou ate mesmo pelo apostrofe.
+
+//console.log(typeof `ola mundo`);
+
+//booleano : representação de valores lógicos, true ou false.
+
+//console.log(typeof false);
+
+//null : representação de valores nulos.
+
+//console.log(typeof null);
+
+//undefined : representação de valores não definidos.
+
+//operadores em javascript
+
+//soma : +
+
+//console.log(1 + 4);
+
+//subtração : -
+
+console.log(4 - 1);
