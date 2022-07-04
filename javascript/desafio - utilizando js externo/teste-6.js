9787d140f7a8c62b1a834bbfed4d9680f62af8b3
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-07-04 14:14:46 -0300

    desafio js externo

---
+++ javascript/desafio - utilizando js externo/teste-6.js
@@ -0,0 +1,18 @@
+//usando javascript externo.
+
+
+let oldPerson = prompt("Coloque o nome da pessoa mais velha");
+
+let ageOldPerson = prompt("Coloque a idade da pessoa mais velha");
+
+let newPerson = prompt("Coloque o nome da pessoa mais nova");
+
+let ageNewPerson = prompt("Coloque a idade da pessoa mais nova");
+
+let ageDifference = ageOldPerson - ageNewPerson;
+
+alert ("O nome da pessoa mais velha é : " + oldPerson + " E a  idade dessa pessoa é: " + ageOldPerson + " anos");
+
+alert ("O nome da pessoa mais nova é : " + newPerson + " E a  idade dessa pessoa é: " + ageNewPerson + " anos");
+
+alert ("A diferença entre as idades das duas pessoas é: " + ageDifference + " anos");
