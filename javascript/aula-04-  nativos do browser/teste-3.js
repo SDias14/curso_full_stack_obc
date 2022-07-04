300ee1937bc8e2453d3f83542295d895a9d2fe7f
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-07-04 09:43:26 -0300

    Update teste-3.js

--- javascript/aula-04-  nativos do browser/teste-3.js
+++ javascript/aula-04-  nativos do browser/teste-3.js
@@ -4,4 +4,31 @@
 
 //console.log("Bem vindo à Nave Elemental");
 
-//console.info
+
+//console.log : retorna a mensagem com cor diferente:
+
+//console.info("Bem vindo à Nave Elemental");
+
+//console.warn : mensagens de urgencia : 
+
+//console.warn("Bem vindo à Nave Elemental");
+
+//console.error : mensagens de erro :
+
+//console.error("Bem vindo à Nave Elemental");
+
+
+//alert : mostra uma mensagem na tela :
+
+//alert("Agendamento realizado com sucesso!");
+
+//confirm : mostra uma mensagem e um botao de cancel e ok :
+
+//confirm("Deseja realmente cancelar?");
+
+//prompt : mostra uma mensagem e um input :
+
+prompt("Digite seu nome: ");
+
+
+
