af1fcbda9f33c3e6a4d9981e6c65c5d7bc2340fc
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-07-05 15:15:35 -0300

    compardores

---
+++ javascript/aula-07-comparações/teste-7.js
@@ -0,0 +1,27 @@
+//comparações
+
+let spaceship = "David";
+let velocity = 100;
+
+console.log(velocity !== " 100 ");
+
+//comparação de diferença com uma exclamação e dois iguais, está perguntando se existe diferença entre valor e tipo.
+
+//comparadores de maior ou igual.
+
+console.log(velocity > 70);
+
+console.log(spaceship > "Bern");
+//compara se a primeira string é maior que a segunda string.
+
+//msaior ou igual
+
+console.log(velocity >= 100);
+
+//menor
+
+    console.log(velocity < 100);
+
+    //menor ou igual
+
+    console.log(velocity <= 100);
