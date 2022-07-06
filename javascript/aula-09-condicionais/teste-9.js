483325eba75b6d77e9435afc397194c3e92ef454
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-07-06 13:46:58 -0300

    condicionais

---
+++ javascript/aula-09-condicionais/teste-9.js
@@ -0,0 +1,37 @@
+//condicionais = base de codigos que sao executados com base numa condição. 
+
+let velocity = 99;
+
+/*
+//if && ifelse
+
+if(velocity <= 90){
+    console.log("Testamos uma velocidade aceitavel");
+} else {
+    console.log("Velocidade acima de 90")
+} // é um famoso controle de fluxo.
+
+*/
+
+//encadeamento com else if 
+
+/*
+
+if(velocity < 40){
+    console.log("velocidade baixa");
+} else if( velocity <= 100){
+        console.log("Cuidado ! Proximo do limite")
+    } else {
+        console.log("velocidade de risco")
+    }
+
+    */
+
+    //operador ternario = if de uma linha 
+
+
+    (velocity > 100 ) ? console.log ("velocidade maior que 100") : console.log("velocidade menor que 100") // o operador ternario vai fazer uma avaliação : se a condição antes da interrogação for verdadeira ele vai executar o primeiro trecho de codigo apos a interrogação. Caso for falso ele vai executar o codigo depois dos dois pontos. Recomenda-se usar o operador ternario apenas quando os blocos de codigo sao pequenos. 
+
+
+
+
