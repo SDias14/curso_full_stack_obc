e996d428a79fd648903caed1ca1cc3f47b33e117
samuel dias <samuel.dias36@etec.sp.gov.br>
2022-07-07 16:40:16 -0300

    inicio do exercicio switch

--- javascript/desafio switch/teste-13.js
+++ javascript/desafio switch/teste-13.js
@@ -1,77 +1,17 @@
-//switch 
+//desafio switch case - orientações : 
 
-/*let velocity = 90 
+//1 - Perguntar a distancia em anos-luz
 
-switch (velocity *2) {
-    case 100:
-        console.log('Velocity is 100')
-        break;
-    case 160:
-        console.log('Velocity is 160')
-        break;
-    default:
-        console.log('Velocity is not 100 or 160')
-        break;
-       }
-*/
+//2- Oferecer uma lista de opções com : Parsec (pc), Unidade Astronomica(AU), quilometro(km)
 
-//switch com string
-/*
-let spaceship = "Defiant"    
+//3 - Perguntar qual operação deseja realizar 
 
-switch (spaceship) {
-    case "Golias":
-        console.log("This is a spaceship from the Star Wars franchise")
-        break;
+//4 - Utilize o switch para verificar a opção escolhida pelo usuario. (Armazenar o nome da unidade de medida escolhida) e (Armazenar o valor convertido de anos-luz para a unidade desejada.)
 
-    case "Enterprise":
-        console.log("This is a spaceship from the Star Trek franchise")
-        break;
-    case "Defiant":
-        console.log("This is a spaceship from the Star Trek franchise")
-        break;
-    default:
-        console.log("This is not a Star Wars or Star Trek spaceship")       
-} */
-//pode- se aninhar varios cases e executar um comando apenas.
+//5 - Exiba com alerta contendo a distancia em anos-luz e a distancia convertida para a operação desejada. 
 
-/*
+//6 - Utilize as seguintes conversoes : 
 
-let velocity = 90
-
-    switch (velocity) {
-        case 80:
-        case 90:
-        case 100:
-            console.log('Velocidade aceitável')
-            break;
-        case 110: 
-         console.log('Velocidade acima do limite')
-         break;
-        default:
-            console.log ("velocidade não identificada")     
-    } // o case vai ate o break, se nao tiver break, vai ate o default, ou seja, o switch testa todas as condições ate achar a verdadeira e parar. 
-
-    */
-
-    let spaceship = "Elemental"
-
-    switch (spaceship) {
-
-        case "Golias":
-            console.log("Nave mais resistente do universo")
-        case "Elemental" :
-            console.log("Nave com melhor armamento do universo")
-        case "Helmet":
-            console.log("Rapida ativação de escudo") 
-        case "Frontier":
-            console.log("Nave de infantaria")     
-            break;  
-        case "Enterprise":
-            console.log("Nave da frota estelar")    
-        default:
-            console.log("Nave não identificada")    
-
-
-
-    }
+//1 l.y = 0,306601pc 
+//1 l.y = 63241,1 AU
+//1 l.y = 9.5 * 10^12km
