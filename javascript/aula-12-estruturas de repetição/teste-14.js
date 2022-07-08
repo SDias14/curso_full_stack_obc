cd330751707259b06e8c0a981bc6ca715d6620ea
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-07-08 15:52:07 -0300

    laços de repetição

---
+++ javascript/aula-12-estruturas de repetição/teste-14.js
@@ -0,0 +1,16 @@
+// estruturas de repetição são usadas para repetir um bloco de código. 
+
+//veremos while, do while, for
+
+//while : 
+
+//while (expressao) {
+    // <bloco de código>
+//}
+
+let velocity = 50;
+let acceleration = 5;
+
+while (velocity <=100){
+    console.log ("Acelerando : Estamos a " + velocity + "km/s" )
+}
