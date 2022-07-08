33027e305e83a6686e3bb967a7e5877a3f1c5462
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-07-08 15:28:08 -0300

    Update teste-13.js

--- javascript/desafio switch/teste-13.js
+++ javascript/desafio switch/teste-13.js
@@ -15,3 +15,23 @@
 //1 l.y = 0,306601pc 
 //1 l.y = 63241,1 AU
 //1 l.y = 9.5 * 10^12km
+
+//inicio 
+
+let lightYear = prompt("Digite a distancia em anos-luz");
+let opcao = prompt("Digite a operação desejada : \n 1 - Para converter para Parsec \n 2 - Para converter para Unidade Astronomica \n 3 - Para converter para Quilometro");
+
+switch (opcao) {
+    case "1":
+        alert(`A distancia em anos-luz é ${lightYear} e convertida para Parsec é ${lightYear * 0.306601}`);
+        break;
+    case "2":
+        alert(`A distancia em anos-luz é ${lightYear} e convertida para Unidade Astronomica é ${lightYear * 63241.1}`)
+        break;
+    case "3":
+        alert(`A distancia em anos-luz é ${lightYear} e convertida para Quilometro é ${lightYear * 9.5 * Math.pow(10, 12)}`)
+        break;
+    default:
+         alert("Opção inválida");                   
+
+}
