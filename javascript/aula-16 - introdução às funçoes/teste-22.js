789f9a1455c972ca67174b5a361989b51f92759d
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-07-13 11:08:21 -0300

    introdução a função

---
+++ javascript/aula-16 - introdução às funçoes/teste-22.js
@@ -0,0 +1,21 @@
+//introdução a funçoes 
+
+// funções são bloco de código que executa uma tarefa especifica
+
+// a função pode ser nomeada, ou seja, pode ser chamada de qualquer nome
+
+/* 
+modelo de função: 
+
+    function nomeDaFuncao(parametros){
+        // código
+    }
+
+    nomeDaFuncao(parametros);// chamando a função
+*/
+
+function greetPilot () {
+    alert ("Olá piloto!");
+}
+
+greetPilot();
