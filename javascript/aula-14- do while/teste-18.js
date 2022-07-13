94b86ee03facca3b702b388d3b2490ccff4b9de0
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-07-13 09:40:45 -0300

    do while

---
+++ javascript/aula-14- do while/teste-18.js
@@ -0,0 +1,12 @@
+// do while
+
+let velocity = 50 
+let acceleration = 5
+
+do {
+console.log("Acelerando... estamos a " + velocity + " km/h")
+velocity += acceleration
+} while (velocity <= 100)
+
+//do while so avalia a expresao no final. 
+
