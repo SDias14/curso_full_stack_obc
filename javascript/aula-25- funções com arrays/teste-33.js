214961fd296a253d23644b20220768ae643ff702
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-07-19 15:25:37 -0300

    arrays introdução

---
+++ javascript/aula-25- funções com arrays/teste-33.js
@@ -0,0 +1,16 @@
+// introdução aos arrays
+
+//arrays sao estruturas de dados que armazenam varios valores em uma unica variavel. dados sao organizados em um indice numerico.
+
+/*
+
+let hitchedSpaceships = ["Millennium Falcon", "X-Wing", "TIE Advanced x1", "Executor"]; // array de strings
+
+
+console.log(hitchedSpaceships);
+
+*/
+
+let countDown = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, "Liftoff!"];
+
+console.log(countDown);
