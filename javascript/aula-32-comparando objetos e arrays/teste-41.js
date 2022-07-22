f9a3fcf449462c5bce42140f562bc5749f361c43
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-07-22 09:10:06 -0300

    diferenças objetos e arrays

---
+++ javascript/aula-32-comparando objetos e arrays/teste-41.js
@@ -0,0 +1,32 @@
+//  comparando objetos e arrays 
+
+/* objeto
+
+
+let spacialStation = {
+    name: 'Receptora',
+    platformsQuantity: 15,
+    shape: 'sphere'
+
+}
+
+
+console.log(spacialStation.name)
+
+
+let UpCasedName = spacialStation.name.toUpperCase()
+
+console.log(UpCasedName)
+
+*/
+
+//array
+
+let spaceshipNames = ['Receptora', 'Nexus', 'Voyager']
+
+let firstSpaceshipUpCased = spaceshipNames[0].toUpperCase()
+
+console.log(firstSpaceshipUpCased)
+
+
+
