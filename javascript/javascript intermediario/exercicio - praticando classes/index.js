7da5865581b4473a68cb2949cf1799d3b6ec071f
samuel dias <samuel.dias36@etec.sp.gov.br>
2022-08-26 16:49:01 -0300

    Update index.js

--- javascript/javascript intermediario/exercicio - praticando classes/index.js
+++ javascript/javascript intermediario/exercicio - praticando classes/index.js
@@ -1,181 +1,77 @@
-/*
-
-Temos uma estação espacial que tem uma lista de naves engatadas. 
-Cada nave possui nome, quantidade de tripulantes, uma informação se está ou nao engatada e outra informando se as portas de entrada estão abertas ou não.
-
-Para gerenciar esses dados, um menu deve ser exibido com as seguintes opções : 
-
-
-1 - REALIZAR ENGATE 
-
-nessa opção deve ser possivel cadastrar uma nave com nome e numero de tripulantes. 
-
-apos o cadastro, a nave pode ser engatada. Quando uma nave é engatada, suas portas tambem sao abertas. Entao, alem de alterar a propriedade da nave que informa se ela esta engatada, altere tambem a propriedade que informa se as portas de entrada estao abertas ou nao.
-
-
-2 - IMPRIMIR TODAS AS NAVEES
-
-a lista de naves deve ser impressa com alert
-
-3   - SAIR DO PROGRAMA 
-
-ao clicar para sair, o programa é encerrado. 
-
-para este exercicio : 
-
-
-crie apenas a classse para a espaçonave 
-
-esta classe deve ter o metodo para engatar a nave
-
-crie um array para armazenar as naves
-
-o programa deve ser encerrado somente quando o usuario clicar em sair 
-
-caso o usuario escolha uma opcao invalida, deve ser mostrado novamente o menu. 
-
-
-*/
-
-// primeiro passso : criar a classe para a espaçonave
-
 class Spaceship {
-    constructor(name, crew, isEngaged = 'engatada', isDoorOpen = 'abertas') {
+    constructor(name, crew) {
         this.name = name;
         this.crew = crew;   
-        this.isEngaged = isEngaged ;
-        this.isDoorOpen = isDoorOpen;
+        this.isHitched = false ;
+        this.isDoorOpen = false;
         
     } 
+
+    hitch() {
+        this.isHitched = true;
+        this.isDoorOpen = true;
+    }
     
     
 }
 
+//primeira parte do programa. Quando o programa rodar ele vai pedir para o usuario escolher uma opção. Essa é uma função que vai retornar uma opção escolhida pelo usuario.
+
+function showMenu() {
+   let chosenOption
+   while (chosenOption !="1" && chosenOption !="2" && chosenOption !="3") {
+       chosenOption = prompt("Escolha uma opção : \n 1 - Realizar engate \n 2 - Imprimir todas as naves \n 3 - Sair do programa");
+   }
+    return chosenOption;
+
+}
+
+function createSpaceShip() {
+    let spaceshipName = prompt("Digite o nome da nave");
+    let spaceshipCrew = prompt("Digite o numero de tripulantes");
+    let spaceship = new Spaceship(spaceshipName, spaceshipCrew);
+    return spaceship;
+}
+
+function printSpaceships(spaceships) {
+    let spaceshipList = "";
+    spaceships.forEach((spaceship, index) => {
+        spaceshipList += `${index + 1} - ${spaceship.name} - ${spaceship.crew} tripulantes \n`;
+    } );
+
+    alert(spaceshipList);
 
 
 
+}
 
-// 2 parte - criação do menu e criação do array para armazenar as naves
+//2 parte - se for escolhido a opção 1, o programa vai chamar a função createSpaceShip para criar uma nova nave.
+
+let hitchedSpaceships = []
+
+let chosenOption
 
 
-let menu = prompt("Escolha uma opção : \n 1 - Realizar engate \n 2 - Imprimir todas as naves \n 3 - Sair do programa");
+while(chosenOption !="3") {
+    chosenOption = showMenu();
+
+    switch (chosenOption) {
+
+        case '1':
+            let spaceshipToAdd = createSpaceShip();
+            spaceshipToAdd.hitch();
+            hitchedSpaceships.push(spaceshipToAdd);
+            break;
+        case '2':
+            printSpaceships(hitchedSpaceships);
+            break;
 
 
 
+}
 
-
-if (menu == 1) {
-   
+}
     
-    let nome = prompt("Digite o nome da nave");
-    let trip = prompt("Digite o numero de tripulantes");
-    
-    let spaceship = new Spaceship(nome, trip);
-
-    alert(`A nave ${spaceship.name} possui ${spaceship.crew} tripulantes e está ${spaceship.isEngaged}`);
-
-
-    
-    //ate aqui ok 
-    
-    let confirmation = prompt ( 'Tem mais alguma nave que queira incluir ? \n digite 1 para continuar digitando  2 para exibir as naves cadastradas e 3 para cancelar');
-    
-    
-    while (confirmation == 1) {
-        nome = nome + ' ' + prompt ( 'Digite o nome da nave')
-        
-        trip = trip + ' ' + prompt ( 'Digite o numero de tripulantes')
-        
-        spaceship = new Spaceship(nome, trip);
-
-        alert(`A nave ${spaceship.name} com ${spaceship.crew} tripulantes foi ${spaceship.isEngaged} e as portas estão ${spaceship.isDoorOpen}`);
-
-        //ok ate aqui
-        
-       confirmation = prompt ( 'Tem mais alguma nave que queira incluir ? \n digite 1 para continuar digitando  2 para exibir as naves cadastradas e 3 para cancelar')
-
-    }
-
-       if (confirmation == 2) {
-           alert(`A nave ${spaceship.name} com ${spaceship.crew} tripulantes foi ${spaceship.isEngaged} e as portas estão ${spaceship.isDoorOpen}`);
-
-              confirmation = prompt ( 'Tem mais alguma nave que queira incluir ? \n digite 1 para continuar digitando  2 para exibir as naves cadastradas e 3 para cancelar')
-
-              //ok ate aqui
-
-              while (confirmation == 1) {
-                  nome = nome + ' ' + prompt ( 'Digite o nome da nave')
-                  
-                  trip = trip + ' ' + prompt ( 'Digite o numero de tripulantes')
-                  
-                  spaceship = new Spaceship(nome, trip);
-
-                  alert(`A nave ${spaceship.name} com ${spaceship.crew} tripulantes foi ${spaceship.isEngaged} e as portas estão ${spaceship.isDoorOpen}`);
-
-                  //ok ate aqui
-                  
-                 confirmation = prompt ( 'Tem mais alguma nave que queira incluir ? \n digite 1 para continuar digitando  2 para exibir as naves cadastradas e 3 para cancelar')
-
-              
-
-                if (confirmation == 2) { 
-                    alert(`A nave ${spaceship.name} com ${spaceship.crew} tripulantes foi ${spaceship.isEngaged} e as portas estão ${spaceship.isDoorOpen}`);
-
-                          confirmation = prompt ( 'Tem mais alguma nave que queira incluir ? \n digite 1 para continuar digitando  2 para exibir as naves cadastradas e 3 para cancelar')
-                } else if (confirmation == 3) {
-                    alert('Obrigado por utilizar o programa');
-                } else {
-                    alert('Opção inválida');
-                }
-            }   
-
-        } else if (confirmation == 3) {
-              alert('Obrigado por utilizar o programa');
-         } else {
-             alert('Opção inválida');           
-         } 
-        
-        
-        } else if (menu == 2) {
-
-            alert('nenhuma nave cadastrada');
-        
-    } else if (menu == 3) {
-        alert('Programa encerrado');
-    } else {
-        alert('Opção inválida');
-    }
-
-        
-
-    
-
-
-
-
-
-
-
-
-
-    
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
