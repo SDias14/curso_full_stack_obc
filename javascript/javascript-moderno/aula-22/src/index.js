d88bc1046a1b1c439d6ad39ba3eae20c910d7f4d
samuel dias <93612090+SDias14@users.noreply.github.com>
2022-12-13 15:05:16 -0300

    webpack e babel

---
+++ javascript/javascript-moderno/aula-22/src/index.js
@@ -0,0 +1,5 @@
+import dayjs from 'dayjs';
+import './styles/index.css';
+
+
+alert(dayjs().format('DD/MM/YYYY'));
---
+++ javascript/javascript-moderno/aula-22/webpack.config.js
@@ -0,0 +1,23 @@
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const path = require('path');
+
+module.exports = {
+entry: {
+    index: './src/index.js',
+},
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader']
+
+        }
+    ]
+},
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
+
+}
