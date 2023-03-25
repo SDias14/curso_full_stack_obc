35d5442821dfaf39e005eb641ac3c29ab644e188
samuel dias <93612090+SDias14@users.noreply.github.com>
2023-03-24 21:56:31 -0300

    todo

--- todo/src/routes/checklist.js
+++ todo/src/routes/checklist.js
@@ -7,5 +7,32 @@ router.get('/', (request, response) => {
     response.send('My todo app');
 });
 
+router.get('/:id', (request, response) => {
+   const id = request.params.id;
+    
+    console.log(id);
+    
+    response.send('the id is: ' + id);
+});
+
+router.post('/', (request, response) => {
+    console.log(request.body);
+    response.status(201).json(request.body);
+});
+
+router.put('/:id', (request, response) => {
+    const id = request.params.id;
+    console.log(id);
+    console.log(request.body);
+    response.status(200).json(request.body);
+});
+
+router.delete('/:id', (request, response) => {
+    const id = request.params.id;
+    console.log(id);
+    response.send('Deleted id: ' + id);
+});
+
+
 module.exports = router;
 
